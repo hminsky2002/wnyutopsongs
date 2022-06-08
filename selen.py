27efc6bbc62fe3fa1b6b226673b61605af3f5f08
hminsky2341 <harry.minsky@gmail.com>
2022-06-08 19:13:54 -0400

    got working for indefinite time range
    Is now working at reasonable speed for scraping all tracks over an indefinite range. Also added dates and show names to the formatting

--- selen.py
+++ selen.py
@@ -1,16 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
-startdate = "2022-05-29"
-enddate = "2022-06-11"
-chrome_path = r"/Users/harryminsky/Downloads/chromedriver"
+import os
+import glob
+import time
 
-def login():
+startdate = "2022-01-24"
+enddate = "2022-05-19"
+directory = "episoderanges/"+startdate+"to"+enddate
+chrome_path = r"/Users/harryminsky/Downloads/chromedriver"
+#fix to get around connection not secure page
+def initDriver(DownloadPath):
+    prefs = {"download.default_directory" : "/Users/harryminsky/PycharmProjects/wnyutopsongs/"+DownloadPath}
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_experimental_option("prefs",prefs)
+    driver = webdriver.Chrome(chrome_path,options=options)
+    return driver
+def login(driver):
     '''logs in to the wnyu webpage, uses tech admin creds'''
     driver.find_element(By.ID, "user_email").send_keys("tech@wnyu.org")
     driver.find_element(By.ID, "user_password").send_keys("Radioradioradio89.1")
     driver.find_element(By.NAME, "commit").click()
-
 def datepick(start,end):
     '''takes a start and end date range as strings in the form YYYY-MM-DD'''
     driver.find_element(By.NAME, "q[start_at_gteq_datetime]").send_keys(start)
@@ -18,43 +30,44 @@ def datepick(start,end):
     driver.find_element(By.NAME, "commit").click()
 
 
+os.mkdir(directory)
 
-
-#fix to get around connection not secure page
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
-driver = webdriver.Chrome(chrome_path,options=options)
-
+driver = initDriver(directory)
 driver.get("https://www.wnyu.org/admin")
-login()
+login(driver)
+# driver.get("https://www.wnyu.org/admin/episodes?order=start_at_desc&scope=with_playlists&utf8=%E2%9C%93")
+# datepick(startdate,enddate)
 #get to playlist page
-driver.get("https://www.wnyu.org/admin/episodes?scope=with_playlists")
+episodes_csv = "https://www.wnyu.org/admin/episodes.csv?order=start_at_desc&q%5Bstart_at_gteq_datetime%5D="+startdate+"&q%5Bstart_at_lteq_datetime%5D="+enddate+"&scope=with_playlists&utf8=%E2%9C%93"
 
-datepick(startdate,enddate)
-#get the list of episodes in the range this won't work for larger ranges right now
-text_contents = [el.text.split(' ') for el in driver.find_elements(By.TAG_NAME, 'tr')]
-
-#construct id list
-ids = []
-for i in range(1,len(text_contents)):
-    ids.append(text_contents[i][0])
+#getthecsvfileforids
+driver.get(episodes_csv)
+time.sleep(3)
+filename = glob.glob(directory+"/*.csv")
+#get the list of ids
+df = pd.read_csv(filename[0])
+df = df.drop(columns=['Description','Start at', 'End at','File url','Created at','Updated at','Image','Old','Comment','Slug','Locked'])
+idnamedate = df.values.tolist()
 
 titles = []
-names = []
-for i in ids:
-  driver.get(f"https://www.wnyu.org/admin/episodes/{int(i)}/tracks")
+artists = []
+showname = []
+date = []
+for i in idnamedate:
+  driver.get(f"https://www.wnyu.org/admin/episodes/{i[0]}/tracks")
   rawtitles = [el.text for el in driver.find_elements(By.CLASS_NAME, 'col-title')]
   del rawtitles[0]
   titles.extend(rawtitles)
-  rawnames = [el.text for el in driver.find_elements(By.CLASS_NAME, 'col-artist_name')]
-  del rawnames[0]
-  names.extend(rawnames)
+  rawartists = [el.text for el in driver.find_elements(By.CLASS_NAME, 'col-artist_name')]
+  del rawartists[0]
+  artists.extend(rawartists)
+  showname.extend([i[1] for x in range(len(rawartists))])
+  date.extend([i[2] for x in range(len(rawartists))])
 
 
-df = pd.DataFrame(list(zip(titles, names)))
-df.to_csv(f"{startdate}_to_{enddate}.csv",encoding='utf-8',header=['title','artist'])
 
+df2 = pd.DataFrame(list(zip(titles, artists,showname,date)))
+df2.to_csv(f"{startdate}_to_{enddate}.csv",encoding='utf-8',header=['title','artist','showname','date'])
 
 
 
--- statgen.py
+++ statgen.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('2022-05-15_to_2022-05-21.csv')
+df = pd.read_csv('episoderanges/2022-05-15_to_2022-05-21.csv')
 df = df.apply(lambda x: x.astype(str).str.lower())
 df2 = df.iloc[: , 1:]
 df2 =  df2.apply(lambda x: x.astype(str).str.lower())
